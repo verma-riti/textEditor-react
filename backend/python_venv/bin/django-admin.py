769b9497cbdb13fbcb0e4683c00947ad0014a4c8
riti verma <ritiverma@ritis-MacBook-Air.local>
2020-05-14 22:06:32 +0530

    deleted virtual env file

--- backend/python_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ritiverma/projects/kool_kanya/text-editor/backend/python_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
